efc803bb9ec61398c4cede8084c48a413c537c74
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-09-20 20:33:38 +0100

    created an attribution Component to display the royalty that built this project

---
+++ src/Attribution.jsx
@@ -0,0 +1,11 @@
+export default function Attribution() {
+  return (
+    <div className="attribution">
+      Made with ❤️ by{" "}
+      <a href="https://x.com/codewithstephen" target="_blank">
+        Stephen
+      </a>
+      .
+    </div>
+  );
+}
