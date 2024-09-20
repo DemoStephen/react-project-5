8d8efd871cbf4d4dbc4329997b059705d88b4113
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-09-20 20:30:23 +0100

    currently working on usestate so I could change what displays on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import Attribution from "./Attribution";
+import Header from "./Header";
+import Keyboard from "./Keyboard";
+import Screen from "./Screen";
+
 export default function App() {
+  const [currentValue, setcurrentValue] = useState(0);
+  const [PreviousValue, setPreviousValue] = useState("Ans");
+  const [theme, setTheme] = useState("");
+
   return (
     <>
-      <h1>Hello World</h1>
+      <main className="d-flex">
+        <Header />
+        <Screen present={currentValue} past={PreviousValue} />
+        <Keyboard />
+      </main>
+      <Attribution />
     </>
   );
 }
